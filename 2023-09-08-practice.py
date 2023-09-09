1a33ac50d559b1379d76c58753f18d19ed07828b
Ahmed <ahmed.m.almousawi@gmail.com>
2023-09-09 01:13:35 -0600

    unsucessfully plotted point 1

--- 2023-09-08-practice.py
+++ 2023-09-08-practice.py
@@ -3,18 +3,18 @@ import pyromat as pm
 import matplotlib.pyplot as plt
 
 water = pm.get('mp.H2O')
-temp_1 = 273.15 + 40 #Kelvin
+t_1 = 273.15 + 40 #Kelvin
 vol = 50 / 1000 #m^3
 p_1 = 2 # Bar
-v_1 = 1 / water.d(T=temp_1, p = p_1) # 1/density, m^3/kg
+v_1 = 1 / water.d(T=t_1, p = p_1) # 1/density, m^3/kg
 
 # Get density, mass, and enthalpy from the above properties.
-den = water.d(T=temp_1, p=p_1)
+den = water.d(T=t_1, p=p_1)
 mass = vol * den
-enthalpy_i = water.h(T=temp_1, p=p_1)
+enthalpy_i = water.h(T=t_1, p=p_1)
 print(mass)
 
-
+t_2 = 273.15 + 100
 
 # Get the final enthalpy after vapourization of all substatnces
 temp_2 = 100 + 273.15
@@ -33,12 +33,14 @@ Tc,pc = water.critical()
 T = np.arange(Tt,Tc,2.5)
 p = water.ps(T)
 dL,dV = water.ds(T=T)
-ax1.plot(1./dL,p,'k')
-ax1.plot(1./dV,p,'k')
+ax1.plot(1/dL,p,'k')
+ax1.plot(1/dV,p,'k')
 
 ax1.set_xscale('log')
 # ax1.set_yscale('log')
-ax1.set_xlim([5e-4, 50])
-ax1.set_ylim([.01,300.])
+ax1.set_xlim([5e-4, 1])
+ax1.set_ylim([.01,400.])
+
+ax1.plot(v_1, t_1, 'r')
 
 plt.show()
